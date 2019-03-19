08501ea2032e37b98cc4fb85898f269d3d600b92
SOYEONG OH <axionoso@gmail.com>
2019-03-19 20:02:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
     array.join(", ").split(" ").insert(-2, "and").join(" ")
   elsif array.length > 3
     array.pop
-    array.join(", ") << ", and "
+    array.join(", ") << ", and " << array[-1]
   end
 end
