b227ca14199628a9155e08cb14a1418d9dfda5ac
SOYEONG OH <axionoso@gmail.com>
2019-03-19 19:49:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,6 @@ def oxford_comma(array)
   if array.length <= 2
     array.join(" and ")
   elsif array.length > 2
-    array.join(", ").split(" ").insert(-2, "and").join(" ")
-  else
-    array.join(", ").split(" ")
+    array.join(", ").split(" ").insert(-2, "and")
   end
 end
