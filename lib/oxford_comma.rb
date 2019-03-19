30934e2e26fb2303112dfc669df1162346bd69c6
SOYEONG OH <axionoso@gmail.com>
2019-03-19 19:44:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length <= 2
     array.join(" and ")
-  elsif array.length < 3
+  elsif array.length < 2
     array.join(", ").split(" ").insert(-2, "and").join(" ")
   end
 end
